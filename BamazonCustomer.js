c9731bd0ecc59eed3fb6505012c9a4d8647d4757
Sean Capelle <seancapelle@gmail.com>
2016-09-09 16:02:52 -0400

    Working on buy function

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,6 +1,8 @@
+//Grab node packages
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+//mySql connection
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -14,23 +16,61 @@ connection.connect(function(err) {
 	console.log("Connected as ID: " + connection.threadId);
 });
 
+//Select all Products
 connection.query('SELECT * FROM Products', function(err, res) {
 	if (err) throw err;
 
 	console.log("~~~~Welcome to Bamazon!~~~~");
-	console.log("What would you like to buy?");
+
+	//Loop to display all items
 	for (var i = 0; i < res.length; i++){
 
-	console.log(res[i].ItemID + " " + res[i].ProductName + " " + res[i].DepartmentName + " $" + res[i].Price + " " + res[i].StockQuantity);	
+	console.log(res[i].ItemID + " " + "'" + res[i].ProductName + "'" + " " + res[i].DepartmentName + " $" + res[i].Price + " x" + res[i].StockQuantity);	
 	}
-	
-})
 
-// inquirer.prompt([{
-//         name: "post",
-//         message: "Would you like to post or bid on an item?"
-    
-//     }]).then(function(answers) {
+	//Get user info
+	buy();
+});
+
+//Prompt user what they would like to buy
+function buy(){
+	
+	inquirer.prompt([{
+	        name: "id",
+	        message: "Which item would you like to buy?"
+	    }, {
+	    	name: "units",
+	    	message: "How many would you like to buy?"
+	    
+	    }]).then(function(answers) {
+	    	
+	    	//Assign input to variables
+	    	var itemSelect = answers.id;
+	    	var numSelect = answers.units;
+
+	    	//Go to itemCheck
+	    	itemCheck(itemSelect, numSelect);
+	    });
+	    
+}
+
+//See if enough items are in stock to fulfill order
+function itemCheck(itemSelect, numSelect){
+
+	//Connect to DB to look at all Products
+	connection.query('SELECT * FROM Products', function(err, res) {
+		if(err) throw err;
+		
+			//Loop through all items
+			for (var i = 0; i < res.length; i++){
+				
+
+				if(itemSelect == res[i].ItemID)
+					console.log(res[i].ProductName);
+				}
+
+		});
+}
        
 //        if (answers.post === "post"){
 //       	post();
