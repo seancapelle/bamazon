9f0b577067ba76383b9f446eb748ccd60adf08bd
Sean Capelle <seancapelle@gmail.com>
2016-09-10 21:38:26 -0400

    Fixed mysql file

--- bamazon.sql
+++ bamazon.sql
@@ -1,31 +1,27 @@
 -- Create the database--
-CREATE DATABASE bamazon;
+CREATE DATABASE BamazonDB;
 
 -- Create the products table--
-CREATE TABLE `bamazon`.`products` (
-  `itemID` INT NOT NULL AUTO_INCREMENT,
-  `productName` VARCHAR(45) NOT NULL,
-  `departmentName` VARCHAR(45) NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `stockQuantity` INT NULL,
-  PRIMARY KEY (`itemID`));
+CREATE TABLE `Products` (
+  `ItemID` INT NOT NULL AUTO_INCREMENT,
+  `ProductName` VARCHAR(45) NOT NULL,
+  `DepartmentName` VARCHAR(45) NOT NULL,
+  `Price` DECIMAL(10,2) NOT NULL,
+  `StockQuantity` INT NULL,
+  PRIMARY KEY (`ItemID`));
 
 
--- Insert values into the products table--
-INSERT INTO `bamazon`.`products`
-(`productName`,
-`departmentName`,
-`price`,
-`stockQuantity`)
+-- Add product--
+INSERT INTO `Products`
+(`ProductName`,`DepartmentName`,`Price`,`StockQuantity`)
 VALUES
-('Dog Collar', 'Pet Supplies', 5.25, 10),
-('Dog Leash', 'Pet Supplies', 6.25, 12),
-('Coffee Maker', 'Home Goods', 20.25, 10),
-('Lamp', 'Home Goods', 10.25, 5),
-('iPhone', 'Tech', 199.99, 8),
-('Keyboard', 'Tech', 20.50, 4),
-('Crayons', 'Art Supplies', 2.99, 15),
-('Sketchbook', 'Art Supplies', 5.99, 20),
-('Shampoo', 'Bath', 4.50, 10),
-('Conditioner', 'Bath', 5.50, 8);
+('Omega Virus', 'Games', 19.99, 64),
+('The Force Awakens', 'Movies', 19.99, 64),
+('Weebles', 'Toys', 5.99, 64),
+('Cool shirts', 'Clothing', 5.99, 64),
+('Blender', 'Housewares', 49.99, 64),
+('Cards Against Humanity', 'Games', 25.99, 64),
+('Star Wars action figures', 'Toys', 5.99, 64),
+('Shades', 'Clothing', 5.99, 64),
+('iPad', 'Electronics', 199.99, 64);
 
